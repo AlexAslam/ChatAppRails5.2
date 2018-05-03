2374f9e946c590cd3c2dd75c4949a91d8784268a
Alex Aslam <alexaslam02@hotmail.com>
2018-05-03 18:43:59 +0500

    attendance summary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass 
+   
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+ 
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
