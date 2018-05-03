dd37aa708745d6e7fe9487065296bb501951a4f2
Alex Aslam <alexaslam02@hotmail.com>
2018-05-03 15:56:25 +0500

    attendance summary

--- config/application.rb
+++ config/application.rb
@@ -26,17 +26,7 @@ module ChatApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.require_master_key = false
     # Don't generate system test files.
     config.generators.system_tests = nil
-    def secret_key_base 
-     if Rails.env.test? || Rails.env.development? 
-       secrets.secret_key_base || Digest::MD5.hexdigest(self.class.name) 
-   else 
-       validate_secret_key_base( 
-         ENV["SECRET_KEY_BASE"] || credentials.secret_key_base || secrets.secret_key_base 
-         ) 
-   end 
-end 
   end
 end
