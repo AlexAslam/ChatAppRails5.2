c8d14a433676292d8956dfe38813ce73566d02e5
Alex Aslam <alexaslam02@hotmail.com>
2018-05-03 15:53:02 +0500

    attendance summary

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,14 @@ module ChatApp
     config.require_master_key = false
     # Don't generate system test files.
     config.generators.system_tests = nil
+    def secret_key_base 
+     if Rails.env.test? || Rails.env.development? 
+       secrets.secret_key_base || Digest::MD5.hexdigest(self.class.name) 
+   else 
+       validate_secret_key_base( 
+         ENV["SECRET_KEY_BASE"] || credentials.secret_key_base || secrets.secret_key_base 
+         ) 
+   end 
+end 
   end
 end
