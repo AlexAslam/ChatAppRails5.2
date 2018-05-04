32979bc9977d0908e6583f4668f8f8f231d4b214
Alex Aslam <alexaslam02@hotmail.com>
2018-05-04 12:45:41 +0500

    removing aws key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,11 +29,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass 
    
   # Generate digests for assets URLs.
-  # config.assets.digest = true
+  config.assets.digest = true
  
-  config.assets.debug = true
+  config.assets.debug = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
